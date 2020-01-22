d81e067f49efacbb2eb0159018e6492aa2768235
canwaykalburim <canwaykalburim@gmail.com>
2020-01-22 23:59:40 +0900

    React

--- ch2_React/8_Webpack/8-1_webpack/main.js
+++ ch2_React/8_Webpack/8-1_webpack/main.js
@@ -1,3 +1,3 @@
-import {mul} from './8-1-1_calc'
+import {mul} from './calc'
 const a = mul(3, 5)
 console.log(a)
--- ch2_React/8_Webpack/8-1_webpack/webpack.config.js
+++ ch2_React/8_Webpack/8-1_webpack/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './8-1-2_main.js',
+  entry: './main.js',
   output: {
     path: path.resolve(__dirname, 'output'),
     filename: "output.js"
