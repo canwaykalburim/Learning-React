ef06d8618e33b08adb8e2970f1a51784a2c1a7f0
canwaykalburim <canwaykalburim@gmail.com>
2020-03-24 13:42:10 +0900

    React Component

---
+++ ch3_React-Component/8_ HowToHandleInputFormInReact/8-2_check-box.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class cBoxForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { check: true }
+  }
+  render() {
+    // 입력 양식에 체크 상태를 지정
+    return (<div>
+      <form onSubmit={e => this.doSubmit(e)}>
+        <label>
+          <input type='checkbox' onChange={e => this.doChange(e)} checked={this.state.check} /> 먹기
+        </label><br />
+        <input type='submit' value='결정' />
+      </form>
+    </div>)
+  }
+  // 체크 박스 클릭했을 때
+  doChange(e) {
+    this.setState({ check: !this.state.check })
+  }
+  // 입력 양식을 전송했을 때
+  doSubmit(e) {
+    e.preventDefault()
+    window.alert(this.state.check ? '먹기' : '먹지 않기')
+  }
+}
+
+ReactDOM.render(<cBoxForm />, document.getElementById('root'))
