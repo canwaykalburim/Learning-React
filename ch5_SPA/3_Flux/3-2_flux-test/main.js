2141af6ec166ffa74d10d70137151662af24fde6
canwaykalburim <canwaykalburim@gmail.com>
2020-06-25 17:00:43 +0900

    SPA

---
+++ ch5_SPA/3_Flux/3-2_flux-test/main.js
@@ -0,0 +1,30 @@
+const electron = require('electron')
+const path = require('path')
+const url = require('url')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+// 일렉트론 라이프 사이클
+let mainWindow
+app.on('ready', createWindow)
+app.on('window-all-closed', () => app.quit())
+app.on('activate', () => {
+  if(mainWindow === null)  createWindow()
+})
+
+// 화면 생성
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    nodeIntegration: true,
+    width: 1200,
+    height: 800,
+  })
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+  mainWindow.on('closed', function() {
+    mainWindow = null
+  })
+}
