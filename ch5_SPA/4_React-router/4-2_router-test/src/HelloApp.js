f31201aea6076bf338938550c7460a1574c98933
canwaykalburim <canwaykalburim@gmail.com>
2020-06-28 00:29:05 +0900

    SPA

---
+++ ch5_SPA/4_React-router/4-2_router-test/src/HelloApp.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+// 리엑트 라우터로 메인 컴포넌트 정의
+
+const HelloApp = () => (
+  <Router>
+    <div style={{margin: 20}}>
+      <Route exact path='/' component={Home} />
+      <Route path='/ko' component={HelloKorean} />
+      <Route path='/ja' component={HelloJapanese} />
+      <Route path='/en' component={HelloEnglish} />
+    </div>
+  </Router>
+)
+
+// 출력할 컴포넌트 정의
+const Home = () => (
+  <div>
+    <h1>HellApp</h1>
+    <p>언어를 선택해주세요.</p>
+    <ul>
+      <li><a href='/ko'>한국어</a></li>
+      <li><a href='/ja'>일본어</a></li>
+      <li><a href='/en'>영어</a></li>
+    </ul>
+  </div>
+)
+
+const HelloKorean = () => (
+  <div>
+    <h1>안녕하세요</h1>
+    <p><a href='/'>뒤로가기</a></p>
+  </div>
+)
+
+const HelloJapanese = () => (
+  <div>
+    <h1>こんにちは</h1>
+    <p><a href='/'>戻る</a></p>
+  </div>
+)
+
+const HelloEnglish = () => (
+  <div>
+    <h1>Hello</h1>
+    <p><a href='/'>Back</a></p>
+  </div>
+)
+
+export default HelloApp
---
+++ ch5_SPA/4_React-router/4-2_router-test/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloApp from './HelloApp';
+import * as serviceWorker from './serviceWorker';
+import './index.css';
+
+ReactDOM.render(<HelloApp />, document.getElementById('root'))
+
+serviceWorker.unregister();
